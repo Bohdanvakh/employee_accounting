18861fb53d4e34c93bca1db1366e259c304a3149
Bohdan Vakh <bohdanvah@gmail.com>
2023-05-10 12:29:04 +0300

    changed employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,28 @@
 class Employee < ApplicationRecord
+  has_many :positions
+  has_many :vacations
+
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :middle_name, presence: true, length: { minimum: 2, maximum: 30 }
+  validates :passport_data, presence: true, length: { is: 9 }
+  validates :date_of_birth, presence: true
+  validates :place_of_birth, presence: true
+  validates :home_address, presence: true
+  validates :employed_on, presence: true
+  validates :salary, presence: true
+  validate :date_of_birth_validation
+  validate :employed_on_valid
+end
+
+def date_of_birth_validation
+  if date_of_birth > Date.today
+    errors.add(:date_of_birth, "Date of birth can't be in the future")
+  end
+end
+
+def employed_on_valid
+  if employed_on > Date.today
+    errors.add(:employed_on, "The employee could not be employed on this date")
+  end
 end
--- db/migrate/20230509133628_add_columns_to_employee.rb
+++ db/migrate/20230509133628_add_columns_to_employee.rb
@@ -1,15 +1,15 @@
 class AddColumnsToEmployee < ActiveRecord::Migration[7.0]
   def change
     change_table :employees do |t|
-      t.integer :number
-      t.string :surname
+      t.string :last_name
       t.string :first_name
-      t.string :patronymic
+      t.string :middle_name
       t.string :passport_data
       t.date :date_of_birth
       t.string :place_of_birth
       t.string :home_address
-      t.date :date_of_employment
+      t.date :employed_on
+      t.decimal :salary, precision: 10, scale: 2
     end
   end
 end
