e3aded635e08a47f18441967ec43681d42cd8a78
Bohdan Vakh <bohdanvah@gmail.com>
2023-05-10 15:45:18 +0300

    changed position validations

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,8 +1,5 @@
 class Position < ApplicationRecord
-  belongs_to :employees
-  has_many :position_histories
-
   validates :name, presence: true, length: { minimum: 8, maximum: 120 }
-  validates :salary, presence: true
+  validates :salary, presence: true, greater_than: 0
   validates :vacation_days, presence: true
 end
