9c4de1c57bf89127694fc1fe2d33758cc403c5ae
Bohdan Vakh <bohdanvah@gmail.com>
2023-05-15 17:10:51 +0300

    Changed Employee model and migration

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,9 +3,15 @@ class Employee < ApplicationRecord
   validates :first_name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :middle_name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :passport_data, presence: true, length: { is: 9 }
-  validates :date_of_birth, presence: true, date: { before: 18.years.ago }
+  validates :date_of_birth, presence: true, timeliness: { before: 18.years.ago }
   validates :place_of_birth, presence: true, format: { with: /\A[a-zA-Z\s]+\z/ }
   validates :home_address, presence: true, format: { with: /\A\d+\s[A-z0-9]+\s[A-z]+\z/ }
-  validates :employed_on, presence: true, date: { before: -> Date.current }
-  validates :salary, presence: true, greater_than: 0
+  validate :employed_on_in_past
+  validate :age_must_be_eighteen_or_above
+end
+
+def employed_on_in_past
+  if employed_on.present? && employed_on >= Date.current
+    errors.add(:employed_on, "must be in the past")
+  end
 end
--- db/migrate/20230509133628_add_columns_to_employee.rb
+++ db/migrate/20230509133628_add_columns_to_employee.rb
@@ -1,6 +1,6 @@
 class AddColumnsToEmployee < ActiveRecord::Migration[7.0]
   def change
-    change_table :employees do |t|
+    create_table :employees do |t|
       t.string :last_name
       t.string :first_name
       t.string :middle_name
@@ -9,7 +9,6 @@ class AddColumnsToEmployee < ActiveRecord::Migration[7.0]
       t.string :place_of_birth
       t.string :home_address
       t.date :employed_on
-      t.decimal :salary, precision: 10, scale: 2
     end
   end
 end
