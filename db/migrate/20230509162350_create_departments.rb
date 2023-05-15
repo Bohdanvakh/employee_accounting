ffad57c5b446cd881a243138ebe1490c8de2ae58
Bohdan Vakh <bohdanvah@gmail.com>
2023-05-15 16:39:34 +0300

    Changed Department migration

--- db/migrate/20230509162350_create_departments.rb
+++ db/migrate/20230509162350_create_departments.rb
@@ -1,14 +1,11 @@
 class CreateDepartments < ActiveRecord::Migration[7.0]
-  def up
+  def change
     create_table :departments do |t|
       t.string :name
       t.string :abbreviation
-      t.integer :manager_id
+      t.references :manager, foreign_key: true
 
       t.timestamps
     end
-  end
-  def down
-    drop_table :departments
   end
 end
