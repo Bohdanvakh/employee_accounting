dbd8de93d399976f5fa9c986e78c2e41bffdd1bb
Bohdan Vakh <bohdanvah@gmail.com>
2023-05-15 15:21:52 +0300

    Changed Position migration and Position model validation

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,5 +1,5 @@
 class Position < ApplicationRecord
   validates :name, presence: true, length: { minimum: 8, maximum: 120 }
-  validates :salary, presence: true, greater_than: 0
+  validates :salary, presence: true, numericality: { greater_than: 0 }
   validates :vacation_days, presence: true
 end
--- db/migrate/20230509141433_add_columns_to_position.rb
+++ db/migrate/20230509141433_add_columns_to_position.rb
@@ -1,8 +1,8 @@
 class AddColumnsToPosition < ActiveRecord::Migration[7.0]
   def change
-    change_table :positions do |t|
+    create_table :positions do |t|
       t.string :name
-      t.decimal :salary, precision: 10, scale: 2
+      t.integer :salary
       t.integer :vacation_days
     end
   end
