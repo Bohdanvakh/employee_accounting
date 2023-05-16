75ad6aebadf526c0e30dde98265f6f2aab27ede8
Bohdan Vakh <bohdanvah@gmail.com>
2023-05-16 13:34:21 +0300

    Changed PositionHistory model and migration

--- app/models/position_history.rb
+++ app/models/position_history.rb
@@ -1,6 +1,4 @@
 class PositionHistory < ApplicationRecord
   validates :started_on, presence: true
   validates :finished_on, presence: true
-  validates :employee_id, uniqueness: { scope: [:position_id] }
-  validates :position_id, presence: true, uniqueness: true
 end
--- db/migrate/20230509162829_create_position_histories.rb
+++ db/migrate/20230509162829_create_position_histories.rb
@@ -3,8 +3,8 @@ class CreatePositionHistories < ActiveRecord::Migration[7.0]
     create_table :position_histories do |t|
       t.date :started_on
       t.date :finished_on
-      t.references :employee, foreign_key: true
-      t.references :position, foreign_key: true
+      t.belongs_to :employee
+      t.belongs_to :position
       t.timestamps
     end
   end
