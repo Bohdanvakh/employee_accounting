f6a5a0a7e14fe39078b661b2a22bc154a35cd8f6
Bohdan Vakh <bohdanvah@gmail.com>
2023-05-16 13:09:09 +0300

    Changed Position vacation_days

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,5 +1,5 @@
 class Position < ApplicationRecord
   validates :name, presence: true, length: { minimum: 8, maximum: 120 }
   validates :salary, presence: true, numericality: { greater_than: 0 }
-  validates :vacation_days, presence: true
+  validates :vacation_days, presence: true, numericality: { only_integer: true }
 end
