fe4f8a1f77868f563120a5b020dfcf3cfa8fd639
Bohdan Vakh <bohdanvah@gmail.com>
2023-05-29 15:47:15 +0300

    Changed Position Pistories

--- app/models/position_history.rb
+++ app/models/position_history.rb
@@ -3,5 +3,28 @@ class PositionHistory < ApplicationRecord
   belongs_to :position
 
   validates :started_on, presence: true
-  validates :finished_on, presence: true
+  validate :last_position_finished, on: :create
+  validate :validate_position_history_overlap
+
+  def last_position_finished
+    employee = Employee.find_by_id(employee_id)
+    position = employee.position_histories.last
+    if position.present? && position.finished_on.nil?
+      errors.add(:base, "The last position is active")
+    end
+  end
+
+  def validate_position_history_overlap
+    employee = Employee.find_by_id(employee_id)
+
+    if employee.position_histories.present? && !employee.position_histories.last.finished_on.nil?
+      employee.position_histories.each do |position|
+        position_interval = position.started_on..position.finished_on
+        if position_interval.cover?(started_on) || position_interval.cover?(finished_on)
+          errors.add(:base, "Position cannot overlap with other positions")
+        end
+      end
+    end
+  end
+
 end
