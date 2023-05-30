928853cf9690b1fe8200afe6c41476c980074f8d
Bohdan Vakh <bohdanvah@gmail.com>
2023-05-30 15:53:33 +0300

    Added validation for number of vacations

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -1,13 +1,14 @@
 class Vacation < ApplicationRecord
   belongs_to :employee
 
-  validates :started_on, presence: true, comparison: { greater_than: :finished_on }
-  validates :finished_on, presence: true
+  validates :started_on, presence: true
+  validates :finished_on, presence: true, comparison: { greater_than: :started_on }
   validates :employee_id, presence: true
   validate :validate_remaining_vacation_days
   validate :validate_not_past_date
   validate :validate_vacation_overlap
   validate :validate_position_active
+  validate :validate_can_take_vacation
 
   def validate_remaining_vacation_days
     remaining_days = employee.vacation_days - employee.used_vacation_days
@@ -36,6 +37,19 @@ class Vacation < ApplicationRecord
     last_position_history = employee.position_histories.last
     if last_position_history.present? && last_position_history.finished_on.present?
       errors.add(:vacation_days, "You can't add vacation because the last position is already finished.")
+    end
+  end
+
+  def validate_can_take_vacation
+    department = employee.department
+    all_employees = department.employees
+    count = 0
+    all_employees.each do |employee|
+      employee.vacations.each do |vacation|
+        vacation_interval = vacation.started_on..vacation.finished_on
+        count += 1 if vacation_interval.cover?(started_on) || vacation_interval.cover?(finished_on)
+        errors.add(:base, "You cannot take a vacation on this dates because 5 employees have already taken it.") if count == 5
+      end
     end
   end
 end
