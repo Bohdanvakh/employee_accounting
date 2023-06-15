c023a013f76d2f26ca35565082ffbceddc6239c3
Bohdan Vakh <bohdanvah@gmail.com>
2023-06-15 13:53:19 +0300

    Fixed abbreviation factory

--- spec/factories/departments.rb
+++ spec/factories/departments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :department do
     sequence(:name) { |n| Faker::Lorem.name }
-    sequence(:abbreviation) { |n| "#{Faker::Lorem.abbreviation}#{n}" }
+    sequence(:abbreviation) { |n| "#{Faker::Lorem.characters}#{n}" }
   end
 end
