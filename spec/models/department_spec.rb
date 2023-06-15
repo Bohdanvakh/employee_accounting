b9bfe57d5efac56b1bb2bf0a1f9969c078c6ceac
Bohdan Vakh <bohdanvah@gmail.com>
2023-06-15 13:52:07 +0300

    Added error_messages test

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Department, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:name).with_message("can't be blank") }
     it { should validate_uniqueness_of(:name) }
-    it { should validate_length_of(:name).is_at_least(6).is_at_most(120) }
+    it { should validate_length_of(:name).is_at_least(6).with_message("is too short (minimum is 6 characters)") }
+    it { should validate_length_of(:name).is_at_most(120).with_message("is too long (maximum is 120 characters)") }
 
-    it { should validate_presence_of(:abbreviation) }
+    it { should validate_presence_of(:abbreviation).with_message("can't be blank") }
     it { should validate_uniqueness_of(:abbreviation) }
-    it { should validate_length_of(:abbreviation).is_at_least(40).is_at_most(400) }
+    it { should validate_length_of(:abbreviation).is_at_least(40).with_message("is too short (minimum is 40 characters)") }
+    it { should validate_length_of(:abbreviation).is_at_most(400).with_message("is too long (maximum is 400 characters)") }
   end
 
   describe 'associations' do
