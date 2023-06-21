903a3ba94db3d97ff3375f8bfcc5bcae89ab5d9c
Bohdan Vakh <bohdanvah@gmail.com>
2023-06-21 12:20:10 +0300

    Added vacation tests, fixed vacation model

--- spec/models/vacation_spec.rb
+++ spec/models/vacation_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Vacation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:started_on) }
+    it { should validate_presence_of(:finished_on) }
+    it { should validate_presence_of(:employee_id) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:employee) }
+  end
 end
