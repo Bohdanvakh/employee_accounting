b9123e246b4871498cc44ae2f0c5fb1bbf4d76fb
Bohdan Vakh <bohdanvah@gmail.com>
2023-06-21 16:50:35 +0300

    Added subject for shoulda-mathers tests

--- spec/models/vacation_spec.rb
+++ spec/models/vacation_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Vacation, type: :model do
   describe "validations" do
+    subject { FactoryBot.create(:vacation) }
+
     it { should validate_presence_of(:started_on) }
     it { should validate_presence_of(:finished_on) }
     it { should validate_presence_of(:employee_id) }
   end
 
   describe "associations" do
+    subject { FactoryBot.create(:vacation) }
+
     it { should belong_to(:employee) }
   end
 end
