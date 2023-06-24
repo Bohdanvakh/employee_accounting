daa232aedb4b8f64a07c8325276f6157c77f198f
Bohdan Vakh <bohdanvah@gmail.com>
2023-06-24 23:47:20 +0300

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  root to: "departments#index"
+  scope "(:locale)", locale: /en|uk/ do
+    root to: "departments#index"
+    # get '/:locale' => 'departments#index'
 
-  resources :positions
-  resources :employees
-  resources :departments
+    resources :positions
+    resources :employees
+    resources :departments
 
-  get "departments/:id/employees", to: "departments#employees"
+    get "departments/:id/employees", to: "departments#employees"
 
-  resources :employees do
-    resources :position_histories, only: [:new, :create, :edit, :update]
-    resources :vacations, only: [:new, :create]
+    resources :employees do
+      resources :position_histories, only: [:new, :create, :edit, :update]
+      resources :vacations, only: [:new, :create]
+    end
   end
 end
