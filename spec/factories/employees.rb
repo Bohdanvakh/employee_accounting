d5adee0b9113bccfa4cd36d155c5f0feb2fc05df
Bohdan Vakh <bohdanvah@gmail.com>
2023-07-07 14:05:57 +0300

    fix: changed factory place_of_birth and fixed tests

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     middle_name { Faker::Name.middle_name }
     passport_data { Faker::Number.number(digits: 9).to_s }
     date_of_birth { Faker::Date.birthday(min_age: 18, max_age: 65) }
-    place_of_birth { Faker::Address.city }
-    home_address { Faker::Address.street_address }
+    place_of_birth { Faker::Address.city.gsub(/[0-9]/, '') }
+    home_address { "123 Main Street" }
 
     association :department
   end
